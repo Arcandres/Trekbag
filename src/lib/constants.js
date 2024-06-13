570ae4fd09c1f614172dec00f8441a2649d96058
Arcandres <Arcandres@gmail.com>
2024-06-12 19:38:42 -0500

    Constants

---
+++ src/lib/constants.js
@@ -0,0 +1,12 @@
+export const initialItems = [
+  { id: 1, name: 'Good mood', packed: true },
+  { id: 2, name: 'Phone charger', packed: false },
+  { id: 3, name: 'Passport', packed: false },
+];
+
+export const buttons = [
+  'Mark all as completed',
+  'Mark all as incomplete',
+  'Reset to initial',
+  'Remove all items',
+];
