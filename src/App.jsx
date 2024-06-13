82a171c552c94042475f9b52af45fed67f95d710
Arcandres <Arcandres@gmail.com>
2024-06-13 16:53:37 -0500

    All methods to create, delete and check items

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,85 @@ import Footer from './Components/Footer';
 import Header from './Components/Header';
 import ItemList from './Components/ItemList';
 import Sidebar from './Components/Sidebar';
+import { useEffect } from 'react';
 
 function App() {
-  const [items, setItems] = useState(initialItems);
-  const [itemText, setItemText] = useState('');
+  const [items, setItems] = useState(
+    () => JSON.parse(localStorage.getItem('items')) || initialItems
+  );
+
+  const totalItems = items.length;
+  const packedItems = items.filter((item) => item.packed === true).length;
+
+  const handleAddItem = (itemName) => {
+    const newItem = {
+      id: crypto.randomUUID(),
+      name: itemName,
+      packed: false,
+    };
+
+    setItems([...items, newItem]);
+  };
+
+  const handleDelete = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+
+    setItems(newItems);
+  };
+
+  const handleToggle = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, packed: !item.packed };
+      }
+
+      return item;
+    });
+
+    setItems(newItems);
+  };
+
+  const handleAllCompleted = () => {
+    const newItems = items.map((item) => ({ ...item, packed: true }));
+
+    setItems(newItems);
+  };
+
+  const handleAllInComplete = () => {
+    const newItems = items.map((item) => ({ ...item, packed: false }));
+
+    setItems(newItems);
+  };
+
+  const handleRemoveAll = () => {
+    setItems([]);
+  };
+
+  const handleReset = () => {
+    setItems(initialItems);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
 
   return (
     <>
       <BackgroundHeading />
-      <main className=''>
-        <Header />
-        <ItemList items={items} setItems={setItems} itemText={itemText} />
+      <main>
+        <Header totalItems={totalItems} packedItems={packedItems} />
+
+        <ItemList
+          items={items}
+          handleDelete={handleDelete}
+          handleToggle={handleToggle}
+        />
         <Sidebar
-          setItems={setItems}
-          itemText={itemText}
-          setItemText={setItemText}
+          handleAddItem={handleAddItem}
+          handleAllCompleted={handleAllCompleted}
+          handleReset={handleReset}
+          handleRemoveAll={handleRemoveAll}
+          handleAllInComplete={handleAllInComplete}
         />
       </main>
       <Footer />
--- src/Components/ButtonGroup.jsx
+++ src/Components/ButtonGroup.jsx
@@ -1,11 +1,34 @@
 import Button from './Button';
-import { buttons } from '../lib/constants';
 
-export default function ButtonGroup() {
+export default function ButtonGroup({
+  onAllCompleted,
+  onRemoveAll,
+  onReset,
+  onAllInComplete,
+}) {
+  const buttons = [
+    {
+      text: 'Mark all as complete',
+      onClick: onAllCompleted,
+    },
+    {
+      text: 'Mark all as incomplete',
+      onClick: onAllInComplete,
+    },
+    {
+      text: 'Reset to initial',
+      onClick: onReset,
+    },
+    {
+      text: 'Remove all items',
+      onClick: onRemoveAll,
+    },
+  ];
+
   return (
     <section className='button-group'>
-      {buttons.map((text, index) => (
-        <Button key={index} type='secondary'>
+      {buttons.map(({ text, onClick }) => (
+        <Button key={onClick.toString()} onClick={onClick} bType='secondary'>
           {text}
         </Button>
       ))}
