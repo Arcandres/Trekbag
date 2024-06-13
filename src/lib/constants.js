eb27ca428a14e427c8ae752005e04d5d3cf2c2c0
Arcandres <Arcandres@gmail.com>
2024-06-13 16:50:51 -0500

    Updated items structure and options for select element added

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,12 +1,11 @@
 export const initialItems = [
-  { id: 1, name: 'Good mood', packed: true },
-  { id: 2, name: 'Phone charger', packed: false },
-  { id: 3, name: 'Passport', packed: false },
+  { id: crypto.randomUUID(), name: 'Good mood', packed: true },
+  { id: crypto.randomUUID(), name: 'Phone charger', packed: false },
+  { id: crypto.randomUUID(), name: 'Passport', packed: false },
 ];
 
-export const buttons = [
-  'Mark all as completed',
-  'Mark all as incomplete',
-  'Reset to initial',
-  'Remove all items',
+export const options = [
+  { value: 'default', label: 'Sort by default' },
+  { value: 'packed', label: 'Sort by packed' },
+  { value: 'unpacked', label: 'Sort by unpacked' },
 ];
